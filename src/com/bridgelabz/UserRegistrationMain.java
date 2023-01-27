aa00281561483a48f68966784e583c961f5ed76a
hansani111 <hansanideshmukh894@gmail.com>
2023-01-28 00:05:02 +0530

    added: atleast one character is in uppercase

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -25,9 +25,9 @@ public class UserRegistrationMain {
         UserDetailsValidation.isValidMobileNumber("7982468891");
 
         System.out.println("----Password----");
-        UserDetailsValidation.isValidPassword("abcdefgh");
-        UserDetailsValidation.isValidPassword("abcd1234");
-        UserDetailsValidation.isValidPassword("abcd12345_@./#&+-");
+        UserDetailsValidation.isValidPassword("Abcdefgh");
+        UserDetailsValidation.isValidPassword("aBcd1234");
+        UserDetailsValidation.isValidPassword("abCD12345!@#$%^&*+()-_=,./");
         UserDetailsValidation.isValidPassword("abcd123");
     }
 }
